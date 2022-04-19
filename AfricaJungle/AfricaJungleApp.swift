38d7df4ec2acd2916dfeb607037a86bfc8106c51
Sarika <sarika.paghdal1@gmail.com>
2022-04-19 15:49:26 +0200

    Initial Commit

---
+++ AfricaJungle/AfricaJungleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaJungleApp.swift
+//  AfricaJungle
+//
+//  Created by Sarika on 19.04.22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaJungleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AfricaJungle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AfricaJungle
+//
+//  Created by Sarika on 19.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

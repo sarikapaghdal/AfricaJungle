4c13990a070a925e3d1077e4e2a62d7ce202266e
Sarika <sarika.paghdal1@gmail.com>
2022-04-19 16:52:29 +0200

    added video player helper

---
+++ AfricaJungle/Helper/VideoPlayerHelper.swift
@@ -0,0 +1,19 @@
+//
+//  VideoHelper.swift
+//  AfricaJungle
+//
+//  Created by Sarika on 19.04.22.
+//
+
+import Foundation
+import AVKit
+
+var videoPlayer: AVPlayer?
+
+func playVideo(fileName: String, fileFormat: String) -> AVPlayer {
+  if Bundle.main.url(forResource: fileName, withExtension: fileFormat) != nil {
+    videoPlayer = AVPlayer(url: Bundle.main.url(forResource: fileName, withExtension: fileFormat)!)
+    videoPlayer?.play()
+  }
+  return videoPlayer!
+}
